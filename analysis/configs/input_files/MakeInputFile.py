b142accd8408f29dc61fa6e31c3e92a947cdf05d
Matthew Nickel <matt.t.nickel@gmail.com>
2020-09-11 23:08:25 +0200

    Updating MakeInputFile

--- analysis/configs/input_files/MakeInputFile.py
+++ analysis/configs/input_files/MakeInputFile.py
@@ -1,6 +1,5 @@
 # MC data at /eos/cms/store/group/phys_top/gkrintir/LbL/GammaGamma2MuMu_GENSIM_woPtCut_PbPb5TeV_v1/crab_GammaGamma2MuMu_GENSIM_woPtCut/200317_235208/0000/
-# Real Data at /eos/cms/store/group/phys_diffraction/lbyl_2018/HIForward_Reco/ntuples/ntuples_data_tracker_branches/HIForward/ntuples_data_lbl_tracker_branches/200317_103751/0000/data_HiForestAOD_1-1.root
-
+# Real Data at/eos/cms/store/group/phys_diffraction/lbyl_2018/HIForward_Reco/ntuples/ntuples_data/HIForward/ntuples_data_lbl/200617_140125/0000/data_HiForwardAOD_1-1.root
 
 import sys
 
@@ -8,16 +7,15 @@ import sys
 runfile = sys.argv[2]
 config = "/afs/cern.ch/user/m/mnickel/private/LightByLight2018/analysis/configs/efficiencies.md"
 input_file = sys.argv[1]
-ending = input_file[(input_file.find("HiForestAOD") + len("HiForestAOD")):]
 
-sig_mu = "/eos/user/m/mnickel/TauTau/BDT_data/BDT_DATA"
-mu_ele = "/eos/user/m/mnickel/TauTau/EMPTY_DATA/EMPTY_MUELE"
-mu_mu = "/eos/user/m/mnickel/TauTau/EMPTY_DATA/EMPTY_MUMU"
+ending = input_file[(input_file.find("data_HiForwardAOD") + len("data_HiForwardAOD")):]
+
+sig_mu = "/eos/user/m/mnickel/TauTau/Exclusivity/Data_Exc"
+mu_ele = "/eos/user/m/mnickel/TauTau/Exclusivity/Data_Exc_Tracks"
 
 f = open(runfile,"a")
 f.write( config + "\n")
 f.write( input_file+ "\n")
 f.write( sig_mu + ending+ "\n")
 f.write( mu_ele + ending + "\n")
-f.write( mu_mu + ending + "\n")
 f.close()
